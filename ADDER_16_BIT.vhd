9a01046b2f02f25973c743d7a3c402a17bfaf4e1
KirthanKamble <125912184+KirthanKamble@users.noreply.github.com>
2023-11-23 15:38:04 +0530

    Add files via upload

--- ADDER_16_BIT.vhd
+++ ADDER_16_BIT.vhd
@@ -5,13 +5,14 @@ entity ADDER_16_BIT is
     port (
         data_A : in std_logic_vector (15 downto 0);
         data_B : in std_logic_vector (15 downto 0);
+        M      : in std_logic;
         data_C : out std_logic_vector (15 downto 0)
     );
 end entity ADDER_16_BIT;
 
 architecture rtl of ADDER_16_BIT is
 
-    signal C_par : std_logic := '0';
+    signal C_par : std_logic;
 
     component FULL_ADDER is
         port (
@@ -21,8 +22,15 @@ architecture rtl of ADDER_16_BIT is
     end component FULL_ADDER;
 
 begin
+
+    C_par <= M; --instanciate M to C_par to be used in addition/subatraction
+
     add_instance: for i in 0 to 15 generate 
-        add_bit: FULL_ADDER port map (A=>data_A(i), B=>data_B(i), Ci=>C_par, S=>data_C(i), Co=>C_par);
+        add_bit: FULL_ADDER port map (A=>data_A(i), 
+                                      B=>(M xor data_B(i)), 
+                                      Ci=>C_par, 
+                                      S=>data_C(i), 
+                                      Co=>C_par);
     end generate add_instance;
 
 end architecture;
