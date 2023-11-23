ec5a6be659e31bff153b56e108097eaeebe1f138
KirthanKamble <125912184+KirthanKamble@users.noreply.github.com>
2023-11-23 22:19:39 +0530

    Add files via upload

---
+++ D_FF.vhd
@@ -0,0 +1,29 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity D_FF is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic;
+        D     : in std_logic;
+        Q     : out std_logic 
+    );
+end entity D_FF;
+
+architecture rtl of D_FF is
+
+begin
+                clock_process: process(clk, reset)
+                begin
+                    if(reset = '0') then 
+                        if(rising_edge(clk)) then
+                            Q <= D;
+                        end if;
+
+                    elsif (reset='1') then
+                        Q <= '0';
+								
+						  end if;
+    end process clock_process;
+
+end architecture;
