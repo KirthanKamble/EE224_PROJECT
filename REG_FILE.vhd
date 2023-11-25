68950c5d91e3e189472915b71144b640517a47cb
KirthanKamble <125912184+KirthanKamble@users.noreply.github.com>
2023-11-25 18:51:10 +0530

    completed reg_file without errors

--- REG_FILE.vhd
+++ REG_FILE.vhd
@@ -10,63 +10,42 @@ entity REG_FILE is
         A3 : in std_logic_vector (15 downto 0);
         A4 : in std_logic_vector (3 downto 0);
 
-        D1 : out std_logic_vetor (15 downto 0);
-        D2 : out std_logic_vetor (15 downto 0);
-        D3 : out std_logic_vetor (15 downto 0);
-        D4 : out std_logic_vetor (15 downto 0);
+        D1 : out std_logic_vector (15 downto 0);
+        D2 : out std_logic_vector (15 downto 0);
+        D3 : out std_logic_vector (15 downto 0);
+        D4 : in std_logic_vector (15 downto 0);
 
-        RF_W : in std_logichello i am rg what to do plz let me do som
+        RF_W : in std_logic;
+		  RF_IP: in std_logic 
     );
 end entity REG_FILE;
 
 architecture rtl of REG_FILE is
 
-    component REGISTER_16_BIT is
-        port (
-            Clk   : in std_logic;
-            Reset : in std_logic;
-            data_in : in std_logic_vector(15 downto 0);
-            data_out : out std_logic_vector(15 downto 0)
-        );
-    end component REGISTER_16_BIT;
-
-    component MUX_8_TO_1 is
-        port (
-            I0 : in std_logic_vector (15 downto 0);
-            I1 : in std_logic_vector (15 downto 0);
-            I2 : in std_logic_vector (15 downto 0);
-            I3 : in std_logic_vector (15 downto 0);
-            I4 : in std_logic_vector (15 downto 0);
-            I5 : in std_logic_vector (15 downto 0);
-            I6 : in std_logic_vector (15 downto 0);
-            I7 : in std_logic_vector (15 downto 0);
-    
-            S  : in std_logic_vector (2 downto 0);
-    
-            Y  : out std_logic_vector (15 downto 0)
-        );
-    end component MUX_8_TO_1;
-
-    component DEMUX_1_TO_8 is
-        port (
-            I : in std_logic_vector (15 downto 0);
-            S : in std_logic_vector (2 downto 0);
-            E : in std_logic;
-    
-            Y0 : out std_logic_vector (15 downto 0);
-            Y1 : out std_logic_vector (15 downto 0);
-            Y2 : out std_logic_vector (15 downto 0);
-            Y3 : out std_logic_vector (15 downto 0);
-            Y4 : out std_logic_vector (15 downto 0);
-            Y5 : out std_logic_vector (15 downto 0);
-            Y6 : out std_logic_vector (15 downto 0);
-            Y7 : out std_logic_vector (15 downto 0)
-        );
-    end component DEMUX_1_TO_8;
+    type Register_Block is array (0 to 7) of std_logic_vector (15 downto 0);
+    signal registers : Register_Block := ((others => (others => '0')));
 
 begin
-    A1: 
+    process(RF_W, A4, D4)
+    begin 
+        if rising_edge(RF_W) then 
+            if RF_W = '1' then 
+                registers(to_integer(unsigned(A4))) <= D4;
+            end if;
+        end if;
+    end process;
+	 
+	 process(RF_IP, A3)
+    begin 
+        if rising_edge(RF_IP) then 
+            if RF_IP = '1' then 
+                registers(7) <= A3;
+            end if;
+        end if;
+    end process;
 
+    D1 <= registers(to_integer(unsigned(A1)));
+    D2 <= registers(to_integer(unsigned(A2)));
+	D3 <= registers(7);
     
-
 end architecture;
