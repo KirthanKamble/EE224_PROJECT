137d652ef710a7de925cae0edcf1676aa10dfde5
KirthanKamble <125912184+KirthanKamble@users.noreply.github.com>
2023-11-25 17:18:00 +0530

    ALU completed without any errors

--- ALU.vhd
+++ ALU.vhd
@@ -13,104 +13,94 @@ end entity ALU;
 
 architecture rtl of ALU is
 
-    component ADDER_16_BIT is
-        port (
-            data_A : in std_logic_vector (15 downto 0);
-            data_B : in std_logic_vector (15 downto 0);
-            M      : in std_logic;
-            data_C : out std_logic_vector (15 downto 0)
-        );
-    end component ADDER_16_BIT;
-
-    component Multiplier is 
-        port (
-            data_A : in std_logic_vector (15 downto 0);
-            data_B : in std_logic_vector (15 downto 0);
-            data_C : out std_logic_vector (15 downto 0)
-        );
-    end component Multiplier;
-
-    component AND_16_BIT is
-        port (
-            data_A : in std_logic_vector (15 downto 0);
-            data_B : in std_logic_vector (15 downto 0);
-            data_C : out std_logic_vector (15 downto 0)
-        );
-    end component AND_16_BIT;
-
-    component OR_16_BIT is
-        port (
-            data_A : in std_logic_vector (15 downto 0);
-            data_B : in std_logic_vector (15 downto 0);
-            data_C : out std_logic_vector (15 downto 0)
-        );
-    end component OR_16_BIT;
-
-    component IMP_16_BIT is
-        port (
-            data_A : in std_logic_vector (15 downto 0);
-            data_B : in std_logic_vector (15 downto 0);
-            data_C : out std_logic_vector (15 downto 0)
-        );
-    end component IMP_16_BIT;
-
-    function perform_addition(A, B : std_logic_vector) return std_logic_vector is
+    function perform_addition(A, B : std_logic_vector (15 downto 0)) return std_logic_vector is
         variable temp_result : std_logic_vector(15 downto 0);
+		  variable C_par       : std_logic := '0';
+		  variable G, P        : std_logic;
     begin
-        ADDER_16_BIT_inst: ADDER_16_BIT port map (A, B, '0', temp_result);
+			add_instance: for i in 0 to 15 loop
+								G := A(i) and B(i);
+								P := A(i) xor B(i);
+								temp_result(i) := P xor C_par;
+								C_par := G or (P and C_par);
+			end loop add_instance;						
+         return temp_result;
+    end function;
+
+    function perform_subtraction(A, B : std_logic_vector(15 downto 0)) return std_logic_vector is
+        variable temp_result : std_logic_vector(15 downto 0);
+		  variable C_par       : std_logic := '1';
+		  variable G, P        : std_logic;
+    begin
+        sub_instance: for i in 0 to 15 loop
+							  G := A(i) and (not B(i));
+							  P := A(i) xor (not B(i));
+							  temp_result(i) := P xor C_par;
+							  C_par := G or (P and C_par);
+		  end loop sub_instance;						  
         return temp_result;
     end function;
 
-    function perform_subtraction(A, B : std_logic_vector) return std_logic_vector is
+    function perform_multiplication(A, B : std_logic_vector(15 downto 0)) return std_logic_vector is
         variable temp_result : std_logic_vector(15 downto 0);
+		  variable product     : std_logic_vector(7 downto 0);
+		  variable A_par       : std_logic_vector(3 downto 0) := A(3 downto 0);
+		  variable B_par       : std_logic_vector(3 downto 0) := B(3 downto 0);
     begin
-        ADDER_16_BIT_inst: ADDER_16_BIT port map (A, B, '1', temp_result);
-        return temp_result;
+        outer: for i in 0 to 3 loop 
+		   inner: for j in 0 to 3 loop
+				product(i+j) := product(i+j) xor (A(i) and B(j)) ;
+			end loop inner;
+		  end loop outer;
+		  
+		  temp_result := "00000000" & produt;
+		  return temp_result;
     end function;
 
-    function perform_multiplication(A, B : std_logic_vector) return std_logic_vector is
+    function perform_and(A, B : std_logic_vector(15 downto 0)) return std_logic_vector is
         variable temp_result : std_logic_vector(15 downto 0);
     begin
-        Multiplier_inst: Multiplier port map (A, B, temp_result);
+        and_instance: for i in 0 to 15 loop
+								temp_result(i) := A(i) and B(i);
+		  end loop and_instance;	
         return temp_result(15 downto 0);
     end function;
 
-    function perform_and(A, B : std_logic_vector) return std_logic_vector is
+    function perform_or(A, B : std_logic_vector(15 downto 0)) return std_logic_vector is
         variable temp_result : std_logic_vector(15 downto 0);
     begin
-        and_inst: AND_16_BIT port map (A, B, temp_result);
+        or_instance: for i in 0 to 15 loop
+								temp_result(i) := A(i) or B(i);
+			end loop or_instance;
         return temp_result(15 downto 0);
     end function;
 
-    function perform_or(A, B : std_logic_vector) return std_logic_vector is
+    function perform_imp(A, B : std_logic_vector(15 downto 0)) return std_logic_vector is
         variable temp_result : std_logic_vector(15 downto 0);
     begin
-        or_inst: OR_16_BIT port map (A, B, temp_result);
+        imp_instance: for i in 0 to 15 loop
+								temp_result(i) := (not A(i)) or B(i);
+			end loop imp_instance;
         return temp_result(15 downto 0);
     end function;
 
-    function perform_imp(A, B : std_logic_vector) return std_logic_vector is
+    function perform_concat(A, B : std_logic_vector(15 downto 0)) return std_logic_vector is
         variable temp_result : std_logic_vector(15 downto 0);
+		  variable A_par       : std_logic_vector(8 downto 0) := A(15 downto 8);
+		  variable B_par       : std_logic_vector(6 downto 0) := B(7 downto 0);
     begin
-        imp_inst: IMP_16_BIT port map (A, B, temp_result);
-        return temp_result(15 downto 0);
-    end function;
-
-    function perform_concat(A, B : std_logic_vector) return std_logic_vector is
-        variable temp_result : std_logic_vector(15 downto 0);
-    begin
-        coancat_inst: conacat port map (A, B, temp_result);
+					  temp_result := A_par & B_par;
         return temp_result(15 downto 0);
     end function;
     
 begin 
     with instr select
-        alu_C <= perform_addition        	(alu_A, alu_B) when 1,
-                 perform_subtraction	    (alu_A, alu_B) when 2,
-                 perform_multiplication     (alu_A, alu_B) when 3,
+        alu_C <= perform_addition    	(alu_A, alu_B) when 1,
+                 perform_subtraction	(alu_A, alu_B) when 2,
+                 perform_multiplication(alu_A, alu_B) when 3,
                  perform_and				(alu_A, alu_B) when 4,
-                 perform_or			    	(alu_A, alu_B) when 5,
-                 perform_imp 				(alu_A, alu_B) when 6,
-                 perfomr_concat 			(alu_A, alu_B) when 7;
+                 perform_or				(alu_A, alu_B)	when 5,
+                 perform_imp 				(alu_A, alu_B)	when 6,
+                 perfomr_concat 			(alu_A, alu_B)	when 7;
 
 end architecture;
