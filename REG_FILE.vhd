a62388bfcbd55045a815e4cb104dcfbd8765a334
Anirudh Jangid <113822790+anirudhjangid2004@users.noreply.github.com>
2023-12-04 04:05:41 +0530

    Final code for Reg File as of 4/12/23 <<aJ>>

--- REG_FILE.vhd
+++ REG_FILE.vhd
@@ -14,26 +14,33 @@ entity REG_FILE is
         D2 : out std_logic_vector (15 downto 0);
         D3 : out std_logic_vector (15 downto 0);
         D4 : in std_logic_vector  (15 downto 0);
+		  
+		  ra : out std_logic_vector (15 downto 0);
+		  rb : out std_logic_vector (15 downto 0);
+		  rc : out std_logic_vector (15 downto 0);
 
         RF_W : in std_logic;
         RF_IP: in std_logic;
-		  clk  : in std_logic
+		  clk  : in std_logic;
+		  rst  : in std_logic
     );
 end entity REG_FILE;
 
 architecture rtl of REG_FILE is
 
     type Register_Block is array (0 to 7) of std_logic_vector (15 downto 0);
-    signal registers : Register_Block := ((others => (others => '0')));
+    signal registers : Register_Block := ((others => "0000000000000000"));
 begin
-    Reg_write : process(RF_W, RF_IP, clk)
+    Reg_write : process(RF_W, RF_IP, clk, rst)
     begin 
         --if (rising_edge(clk)) then
-		  if (RF_w = '1') then
+		if (RF_w = '1' and rst = '0') then
             registers(to_integer(unsigned(A4))) <= D4;
-			else
-			registers(to_integer(unsigned(A4))) <= registers(to_integer(unsigned(A4)));
-        end if;
+		elsif(rst = '1') then
+			for i in 0 to 7 loop
+			registers(i) <= "0000000000000000";
+			end loop;
+      end if;
 		  
 		  if (RF_IP = '1') then
             registers(7) <= A3;
@@ -58,5 +65,8 @@ begin
     D1 <= registers(to_integer(unsigned(A1)));
     D2 <= registers(to_integer(unsigned(A2)));
     D3 <= registers(7);
+	 ra <= registers(0);
+	 rb <= registers(1);
+	 rc <= registers(2);
     
 end architecture;
